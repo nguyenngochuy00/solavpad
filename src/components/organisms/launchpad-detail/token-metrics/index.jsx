85d232b8a0c59ffaf62d4bd243730a56854123bb
tien.nguyenviet <tien.nguyenviet@petronas.com>
2024-02-28 23:06:27 +0700

    Add UI tab token metrics

--- src/components/organisms/launchpad-detail/token-metrics/index.jsx
+++ src/components/organisms/launchpad-detail/token-metrics/index.jsx
@@ -1,8 +1,30 @@
+import { ArcElement, Chart as ChartJS, Legend, Tooltip } from 'chart.js';
+import { Col, Row } from 'react-bootstrap';
+import { Doughnut } from 'react-chartjs-2';
 import "./index.scss";
 
-const SolLaunchpadDetailTokenMetrics = () => {
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const SolLaunchpadDetailTokenMetrics = ({ data }) => {
     return <div className="sol-launchpad-detail-token-metrics">
-        {'// Comingsoon'}
+        <Row className='justify-content-center'>
+            <Col lg="6">
+                <Doughnut
+                    redraw
+                    data={data}
+                    options={{
+                        plugins: {
+                            legend: {
+                                position: 'right',
+                                labels: {
+                                    color: 'white'
+                                }
+                            }
+                        }
+                    }}
+                />
+            </Col>
+        </Row>
     </div>
 }
 export default SolLaunchpadDetailTokenMetrics
