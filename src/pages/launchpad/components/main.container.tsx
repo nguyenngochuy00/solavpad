c2d0ba7f2b88f97ec8ad048a8d4833e82ea83553
Dacdd <im3d.11399@gmail.com>
2024-03-26 20:07:24 +0700

    update

--- src/pages/launchpad/components/main.container.tsx
+++ src/pages/launchpad/components/main.container.tsx
@@ -6,15 +6,20 @@ import SolLaunchpadCompletedContainer from './completed.container';
 import SolLaunchpadOpeningContainer from './opening.container';
 import SolLaunchpadUpcomingContainer from './upcoming.container';
 
-const SolLaunchpadMainContainer = ( projects: ProjectDetail[]) => {
+interface SolLaunchpadMainContainerProps {
+	projects: ProjectDetail[];
+}
+
+const SolLaunchpadMainContainer = ({
+	projects
+}: SolLaunchpadMainContainerProps) => {
 	//get project from api
 	//split project to opening, upcoming, completed
 
-
 	return (
 		<SolLaunchpadTemplate
-			banner={<SolLaunchpadBannerContainer/>}
-			section1={<SolLaunchpadOpeningContainer  />}
+			banner={<SolLaunchpadBannerContainer />}
+			section1={<SolLaunchpadOpeningContainer />}
 			section2={<SolLaunchpadUpcomingContainer />}
 			section3={<SolLaunchpadCompletedContainer />}
 		/>
