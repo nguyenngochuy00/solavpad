9406868776fa34f700cdaae84f4ed3100c40a760
DinoMint <dino@MacBook-Pro-cua-apple.local>
2024-03-28 09:25:05 +0700

    format code

--- src/hooks/updater.tsx
+++ src/hooks/updater.tsx
@@ -1,58 +1,56 @@
-import { useCallback, useEffect, useState } from "react"
-import { useDispatch } from "react-redux"
-import { solaUtils } from "../services/blockchain";
-import { SET_LATEST_BLOCK_NUMBER } from "../redux/types/application";
-import useDebounce from "./useDebounce";
-
+import { useCallback, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { solaUtils } from '../services/blockchain';
+import { SET_LATEST_BLOCK_NUMBER } from '../redux/types/application';
+import useDebounce from './useDebounce';
+import { setLastBlockNumber } from '../redux/actions/applicationAction';
 
 const delayTime = 15; //seconds
 
 export default function AppUpdater() {
-    
-    const dispatch = useDispatch();
-    const provider = solaUtils.getConnectionProvider()
+	const dispatch = useDispatch();
+	const provider = solaUtils.getConnectionProvider();
 
-    const [state, setState] = useState({ blockNumber: 0})
+	const [state, setState] = useState({ blockNumber: 0 });
 
-    const blockNumberCallback = useCallback((blockNumber: number) => {
+	const blockNumberCallback = useCallback(
+		(blockNumber: number) => {
+			setState(s => {
+				console.log('BlockNumber:', Math.max(blockNumber, s.blockNumber));
+				return { blockNumber: Math.max(blockNumber, s.blockNumber) };
+			});
+		},
+		[setState]
+	);
 
-        setState((s) => {
-            console.log("BlockNumber:", Math.max(blockNumber, s.blockNumber));
-             return {  blockNumber: Math.max(blockNumber, s.blockNumber) }
-        })
+	const debouncedState = useDebounce(state, delayTime * 1000);
 
-    },[setState])
+	useEffect(() => {
+		if (!provider) return undefined;
 
+		provider
+			.getSlot()
+			.then(blockNumberCallback)
+			.catch(error => console.error('Failed to get block number', error));
 
-    const debouncedState = useDebounce(state, delayTime*1000)
+		// const subId = provider.onSlotUpdate((data)=>{
+		//  if(data.type !== "completed") return;
+		//     blockNumberCallback(data.slot)
+		// })
+		// provider.removeSlotUpdateListener(subId)
+	}, [dispatch, blockNumberCallback, provider, debouncedState]);
 
-    useEffect(() => {
-       
-        if (!provider) return undefined
-       
-        provider.getSlot().then(blockNumberCallback).catch((error) => console.error('Failed to get block number', error))
-       
-        // const subId = provider.onSlotUpdate((data)=>{    
-        //  if(data.type !== "completed") return;
-        //     blockNumberCallback(data.slot)
-        // })  
-        // provider.removeSlotUpdateListener(subId)
-       
-    }, [dispatch, blockNumberCallback, provider, debouncedState])
+	useEffect(() => {
+		if (debouncedState.blockNumber === 0) return;
+		console.log('BlockNumber latest:', debouncedState.blockNumber);
+		// dispatch({
+		// 	type: SET_LATEST_BLOCK_NUMBER,
+		// 	data: debouncedState.blockNumber
+		// });
+		dispatch(setLastBlockNumber(debouncedState.blockNumber));
 
-  
+		// dispatch(setLastBlockNumber(debouncedState.blockNumber))
+	}, [provider, dispatch, debouncedState.blockNumber]);
 
-
-    useEffect(() => {
-        if (debouncedState.blockNumber === 0) return;
-        console.log("BlockNumber latest:", debouncedState.blockNumber);
-        dispatch({ type: SET_LATEST_BLOCK_NUMBER, data:debouncedState.blockNumber })
-        
-        // dispatch(setLastBlockNumber(debouncedState.blockNumber))
-    }, [provider, dispatch, debouncedState.blockNumber])
-
-
-
-
-    return null
+	return null;
 }
