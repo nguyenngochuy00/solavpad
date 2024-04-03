765d92f420bd4193456c5cf83b7bbdfe5a32ff6d
Dacdd <im3d.11399@gmail.com>
2024-04-03 09:46:51 +0700

    add blockNumber recall list launchpad

--- src/pages/launchpad/index.tsx
+++ src/pages/launchpad/index.tsx
@@ -2,18 +2,19 @@ import { useEffect, useState } from "react";
 import SolLaunchpadMainContainer from "./components/main.container";
 import { ProjectDetail } from "../../types";
 import { getIdoProjects } from "../../redux/services/project";
+import { useBlockLatest } from "../../hooks/useState";
 
 const SolLaunchpadPage = () => {
     //get project from api and pass it to the main container component
     const [projects, setProjects] = useState<ProjectDetail[]>([]);
-
+	const blockNumber = useBlockLatest();
     useEffect(()=>{
         getIdoProjects().then((data) => {
             setProjects(data)
         }).catch((error) => {
             console.log(error);
         })
-    },[])
+    },[blockNumber])
 
     
     return <SolLaunchpadMainContainer projects={projects}/>
